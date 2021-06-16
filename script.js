5f0ea6462b0ea631835c86b159fa7595ff28715e
Lapto-Vu <smallcounter@gmail.com>
2021-06-16 15:54:34 +0900

    updated

--- script.js
+++ script.js
@@ -1,49 +1,24 @@
-class ObjectUtilities {
-    mergeObjects = (pine, apple) => {
-      return { ...pine, ...apple };
-    };
-    removePassword = (potato) => {
-      const { password, ...rest } = potato;
-      return rest;
-    };
-  
-    freezeObj = (tomato) => {
-      return Object.freeze(tomato);
-    };
-  
-    getOnlyValues = (water) => {
-      return Object.values(water);
-    };
-  
-    getOnlyProperties = (melon) => {
-      return Object.keys(melon);
-    };
-  }
-  
-  const ObjectUtilitie = new ObjectUtilities();
-  
-  const objA = {
-    name: "Nicolas",
-    favFood: "Kimchi"
-  };
-  
-  const objB = {
-    password: "12345"
-  };
-  
-  const user = ObjectUtilitie.mergeObjects(objA, objB);
-  console.log(user);
-  
-  const cleanUser = ObjectUtilitie.removePassword(user);
-  console.log(cleanUser);
-  
-  const frozenUser = ObjectUtilitie.freezeObj(cleanUser);
-  
-  const onlyValues = ObjectUtilitie.getOnlyValues(frozenUser);
-  console.log(onlyValues);
-  
-  const onlyProperties = ObjectUtilitie.getOnlyProperties(frozenUser);
-  console.log(onlyProperties);
-  
-  frozenUser.name = "Hello!"; // This should show an error
-  
+class ArrayUtilities {
+  static addZeros = (array) => array.map(v => v*10 )
+  static moreThanFifty = (array) => array.filter(v => v>50)
+  static removeFirst = (array) => array.filter((n,i) => i>0)
+  static sumAll = (array) => array.reduce((acc, v) => acc+v)
+  static divide = (number) => number.toString().split(""); 
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const addZeros = ArrayUtilities.addZeros(numbers);
+console.log(addZeros);
+
+const moreThanFifty = ArrayUtilities.moreThanFifty(addZeros);
+console.log(moreThanFifty);
+
+const noFirst = ArrayUtilities.removeFirst(moreThanFifty);
+console.log(noFirst);
+
+const sumAll = ArrayUtilities.sumAll(noFirst);
+console.log(sumAll);
+
+const divided = ArrayUtilities.divide(sumAll);
+console.log(divided);
